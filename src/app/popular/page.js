ecfb03dd2ca56a156cdbcc58f1ff13f1f2131169
Asp1kkk <a5p1k@yandex.ru>
2024-03-22 19:46:46 +0300

    hook integrated

--- src/app/popular/page.js
+++ src/app/popular/page.js
@@ -1,11 +1,14 @@
-import { getGamesByCategory } from "@/src/api/api-utils";
-import CardList from "/src/components/CardList/CardList";
+"use client";
 
-const New = async () => {
-	const popularCards = await getGamesByCategory("popular");
+import { useGetDataByCategory } from "@/src/api/api-hooks";
+import CardList from "/src/components/CardList/CardList";
+import { Preloader } from "@/src/components/Preloader/Preloader";
+
+const New = () => {
+	const popularCards = useGetDataByCategory("popular");
 	return (
 		<main className="main-inner">
-			<CardList data={popularCards} id="popular" title="Популярные" />
+			{popularCards ? <CardList data={popularCards} id="popular" title="Популярные" /> : <Preloader />}
 		</main>
 	);
 };
