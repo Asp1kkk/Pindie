d26c0b6904013abe017564a48b61f346cb2bf8e7
Asp1kkk <a5p1k@yandex.ru>
2024-03-25 13:10:21 +0300

    useStore integrated

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,51 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import { getJWT, getMe, removeJWT, setJWT } from "../api/api-utils";
-import { AuthContext } from "../context/app-context";
+import { useStore } from "../store/app-store";
 
 export const App = ({ children }) => {
-	const [isAuth, setIsAuth] = useState(false);
-	const [user, setUser] = useState(null);
-	const [token, setToken] = useState("");
-
-	const login = (user, token) => {
-		setIsAuth(true);
-		setUser(user);
-		setToken(token);
-		setJWT(token);
-	};
-
-	const logout = () => {
-		setIsAuth(false);
-		setUser(null);
-		setToken("");
-		removeJWT();
-	};
-
-	const checkAuth = async () => {
-		const token = getJWT();
-		if (token) {
-			const me = await getMe(token);
-			if (me) {
-				login(me, token);
-			} else {
-				logout();
-			}
-		}
-	};
+	const { checkAuth } = useStore();
 
 	useEffect(() => {
 		(async () => await checkAuth())();
 	}, []);
 
 	return (
-		<AuthContext.Provider value={{ isAuth, user, token, login, logout }}>
+		<>
 			<Header />
 			{children}
 			<Footer />
-		</AuthContext.Provider>
+		</>
 	);
 };
